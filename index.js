dcbc8bf980f9e34efc07ab17e5778be7962cf2e9
flaviusmeghesan <flv.meghe@gmail.com>
2024-03-17 22:09:32 +0200

    edited roll

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const myButton = document.getElementById('myButton');
 const myLabel = document.getElementById('myLabel');
+const myLabel2 = document.getElementById('myLabel2');
+
 
 myButton.addEventListener('click', () => {
     let randomNR = Math.floor(Math.random() *6)+1;
   myLabel.textContent = `You rolled a ${randomNR}.`;
+  myLabel2.textContent = `You rolled a ${randomNR}.`;
+
 });
